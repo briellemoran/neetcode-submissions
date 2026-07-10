4520777c8449ae09def4d0e7dacfb9cf31fb5706
Brielle Moran <109609834+briellemoran@users.noreply.github.com>
2026-07-09 22:32:57 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> counts = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            counts.put(s.charAt(i), counts.getOrDefault(s.charAt(i), 0) + 1);
+            counts.put(t.charAt(i), counts.getOrDefault(t.charAt(i), 0) - 1);
+        }
+
+        for (int count : counts.values()) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
